0bb9d1f334e0c95d19fa8d58fbfae7bef8bd44f3
ktpog <ktran26@mail.sfsu.edu>
2021-03-09 13:51:08 -0800

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site is under construction...
           Khang Tran
         </p>
         <a
