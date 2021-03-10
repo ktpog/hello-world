19a5c9e977218e96170c57a2f005d4551f390787
ktpog <ktran26@mail.sfsu.edu>
2021-03-09 21:46:47 -0800

    test

--- src/App.js
+++ src/App.js
@@ -7,17 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Site is under construction...
-          Khang Tran
+          Site is under construction...<br />
+          -- Khang Tran
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
